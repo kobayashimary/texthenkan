05618f127719693564ead4067fa4ad81be59fc45
EC2 Default User <ec2-user@ip-172-31-42-103.ap-northeast-1.compute.internal>
2019-06-27 09:04:53 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'toppages/index'
+  get 'toppages#index'
   root to: 'toppages#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
